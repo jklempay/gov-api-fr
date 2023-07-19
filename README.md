731d5b1a85f5ba04a923db3a1502cf2068d92702
Jack Klempay <jklempay@gmail.com>
2023-07-19 09:51:46 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# gov-api-fr
+ A simple script to download and extract text from PDFs using the GovInfo API and PyMuPDF. Currently only works for the Federal Register but can easily be adapted for other collections.
+ 
+ This solution is rough-and-ready but functional. Plans to add exception handling and some degree of parallelization using the asyncio library.
+ 
+*** **For this script to run you need to obtain a GovInfo API key and set it as an environment variable. Information about the GovInfo API is available [here](https://api.govinfo.gov/docs/).***
+
---
+++ get_fr_text.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import requests
+import fitz
+import os
+
+ENDPOINT_URL = 'https://api.govinfo.gov/published/1936-01-01?offsetMark=*&pageSize=1000&collection=FR&docClass=FR'
+CONTENT_URL = 'https://www.govinfo.gov/content/pkg/'
+API_KEY = os.environ['GOV_API_KEY']
+
+# Create data directory
+if not os.path.isdir('data'):
+  os.mkdir('data')
+
+# Get a list of all issues by looping through all pages of results from API query
+print("Getting list of issues...")
+
+url = ENDPOINT_URL + f'&api_key={API_KEY}'
+issues = []
+
+while True:
+    r = requests.get(url)
+    data = r.json()
+    issues.extend(package['dateIssued'] for package in data['packages'])
+    if data['nextPage']:
+        url = data['nextPage'] + f'&api_key={API_KEY}'
+    else:
+        break
+  
+# Get information about the total number of tasks and completed tasks to track progress
+total_issues = len(issues)
+local_issues = len(os.listdir('data'))
+
+# Extract text from each issue and save to disk
+for issue in issues:
+
+    if not os.path.exists(f'data/{issue}.txt'):
+    
+        url = CONTENT_URL + f'FR-{issue}/pdf/FR-{issue}.pdf'
+        r = requests.get(url)
+        f = r.content
+    
+        with fitz.open(stream=f, filetype='pdf') as doc:
+            text = ''.join(page.get_text() for page in doc)
+            
+        with open (f'data/{issue}.txt', 'w', encoding='utf8') as f:
+            f.write(text)
+      
+    local_issues += 1
+    print(f"Done with {local_issues} out of {total_issues} issues", end = '\r')
